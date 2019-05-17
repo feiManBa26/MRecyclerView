d3129c96756ea26bd826ab44777d90aba7e12629
mr.zang <13803895859@163.com>
2019-05-17 08:57:36 +0800

    初始化 项目

---
+++ app/src/main/java/com/joyssom/widget/MainActivity.java
@@ -0,0 +1,19 @@
+package com.joyssom.widget;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ * author: mr.zang
+ * date：2019-05-17
+ * description:
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
